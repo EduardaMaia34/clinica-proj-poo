234d6804ffe80ed8e0f55a52deaaf0100b2272e1
Lucas-as03 <la79331@gmail.com>
2025-05-21 17:37:56 -0300

    Update Consultas.java

--- Consultas.java
+++ Consultas.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Consultas{
 private int consultaid;
 private int pacienteid;
 private int medicoid;
 private String data;
 private String hora;
+
+Scanner read = new Scanner(System.in);
 
 public void setConsulta(int consultaid){
     this.consultaid = consultaid;
@@ -37,7 +41,7 @@ public String getHora(){
     return hora;
 }
 
-public Consultas(int consultaid, int pacienteid, int medicoid, String data, String hora){
+public Consultas(int consultasid, int pacienteid, int medicoid, String data, String hora){
     setConsulta(consultaid);
     setPaciente(pacienteid);
     setMedico(medicoid);
@@ -46,13 +50,30 @@ public Consultas(int consultaid, int pacienteid, int medicoid, String data, Stri
 }
 
 public void marcar(int pacienteid, int medicoid, String data, String hora){
-    //fazer codigo
+    System.out.println("digite o id do paciente, o id do médico, a data e a hora que deseja agendar: ");
+    this.pacienteid = read.nextInt();
+    this.medicoid = read.nextInt();
+    this.data = read.nextLine();
+    this.hora = read.nextLine();
+    System.out.println("consulta marcada! ");
+    System.out.println("ID do médico: " + medicoid);
+    System.out.println("Data: " + data + "\nHora: " + hora);
 }
 public void cancelar(int consultaid){
-    // fazer codigo
+    System.out.println("Digite o ID da consulta para realizar o cancelamento: ");
+    this.consultaid = read.nextInt();
+    // if consultaid esta no banco de dados
+    System.out.println("Consulta " + consultaid + " cancelada!");
+    // caso contrario, System.out.println("ID inválido, verifique se foi realizado o agendamento");
 }
-public int buscar(int pacienteid, int medicoid, String data, String hora){
-    // fazer codigo
+public void buscar(int pacienteid, int medicoid, String data, String hora){
+    System.out.println("para buscar uma consulta, digite o id do paciente, id do médico, data e hora agendada: ");
+    this.pacienteid = read.nextInt();
+    this.medicoid = read.nextInt();
+    this.data = read.nextLine();
+    this.hora = read.nextLine();
+    // se o id do paciente, id do médico, data e hora estiverem no banco de dados, a consulta aparece na tela
+    // caso contrario, aparece "dados errados ou inexistentes"
 }
 
 }
