b6d60fd04e5a1ddf56ef5ec894df94a4cede1393
Lucas-as03 <la79331@gmail.com>
2025-05-21 16:54:39 -0300

    Consultas.java

---
+++ Consultas.java
@@ -0,0 +1,58 @@
+public class Consultas{
+private int consultaid;
+private int pacienteid;
+private int medicoid;
+private String data;
+private String hora;
+
+public void setConsulta(int consultaid){
+    this.consultaid = consultaid;
+}
+public void setPaciente(int pacienteid){
+    this.pacienteid = pacienteid;
+}
+public void setMedico(int medicoid){
+    this.medicoid = medicoid;
+}
+public void setData(String data){
+    this.data = data;
+}
+public void setHora(String hora){
+this.hora = hora;
+}
+
+public int getConsulta(){
+    return consultaid;
+}
+public int getPaciente(){
+    return pacienteid;
+}
+public int getMedico(){
+    return medicoid;
+}
+public String getData(){
+    return data;
+}
+public String getHora(){
+    return hora;
+}
+
+public Consultas(int consultasid, int pacienteid, int medicoid, String data, String hora){
+    setConsulta(consultaid);
+    setPaciente(pacienteid);
+    setMedico(medicoid);
+    setData(data);
+    setHora(hora);
+}
+
+public void marcar(int pacienteid, int medicoid, String data, String hora){
+    //fazer codigo
+}
+public void cancelar(int consultaid){
+    // fazer codigo
+}
+public int buscar(int pacienteid, int medicoid, String data, String hora){
+    // fazer codigo
+}
+
+}
