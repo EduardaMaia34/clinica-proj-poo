9763a669fd1cb4818bf4cd54b1c0653a130db97d
eduardamaia <eduardamfmcavalcanti@gmail.com>
2025-05-22 11:57:06 -0300

    update em medico e paciente (get e set)

--- Medico.java
+++ Medico.java
@@ -7,12 +7,12 @@ public class Medico {
     private int id;
 
     public Medico(String nome, String cpf, String endereco, double valorConsulta, int codigoConselho, int id) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.endereco = endereco;
-        this.valorConsulta = valorConsulta;
-        this.codigoConselho = codigoConselho;
-        this.id = id;
+        setNome(nome);
+        setCpf(cpf);
+        setEndereco(endereco);
+        setValorConsulta(valorConsulta);
+        setCodigoConselho(codigoConselho);
+        setId(id);
     }
 
     public void cadastro(String nome, String cpf, double valorConsulta, int codigoConselho) {
@@ -21,12 +21,60 @@ public class Medico {
     public void editar(String nome, String cpf, String endereco, double valorConsulta, int codigoConselho) {
         //editar informacoes
     }
+
     public void excluir(int id) {
         //excluir no bd
     }
     public int buscar(int codigoConselho, String nome){
         //buscar no bd
         return 0;
+    }
+
+    //getters e setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getValorConsulta() {
+        return valorConsulta;
+    }
+    public void setValorConsulta(double valorConsulta) {
+        if (valorConsulta > 0.00){
+            this.valorConsulta = valorConsulta;
+        } else {
+            this.valorConsulta = 0.00;
+        }
+    }
+
+    public int getCodigoConselho() {
+        return codigoConselho;
+    }
+    public void setCodigoConselho(int codigoConselho) {
+        this.codigoConselho = codigoConselho;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
     
 }
--- Paciente.java
+++ Paciente.java
@@ -6,11 +6,11 @@ public class Paciente {
     private int id;
 
     public Paciente(String nome, String cpf, String endereco, String prontuario, int id) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.endereco = endereco;
-        this.prontuario = prontuario;
-        this.id = id;
+        setNome(nome);
+        setCpf(cpf);
+        setEndereco(endereco);
+        setProntuario(prontuario);
+        setId(id);
     }
 
     public void cadastro(String nome, String cpf, String endereco, String prontuario) {
@@ -25,6 +25,42 @@ public class Paciente {
     public int buscar(String nome, String cpf){
         //buscar no bd
         return 0;
+    }
+
+    //getters e setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getProntuario() {
+        return prontuario;
+    }
+    public void setProntuario(String prontuario) {
+        this.prontuario = prontuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
     
 }
