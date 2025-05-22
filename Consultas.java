6969078ea7ad87baf7e6f066527836ebd5f4e678
Lucas-as03 <la79331@gmail.com>
2025-05-22 10:32:55 -0300

    Update Consultas.java

--- Consultas.java
+++ Consultas.java
@@ -41,7 +41,7 @@ public String getHora(){
     return hora;
 }
 
-public Consultas(int consultasid, int pacienteid, int medicoid, String data, String hora){
+public Consultas(int consultaid, int pacienteid, int medicoid, String data, String hora){
     setConsulta(consultaid);
     setPaciente(pacienteid);
     setMedico(medicoid);
@@ -49,31 +49,36 @@ public Consultas(int consultasid, int pacienteid, int medicoid, String data, Str
     setHora(hora);
 }
 
-public void marcar(int pacienteid, int medicoid, String data, String hora){
+public void marcar(){
     System.out.println("digite o id do paciente, o id do médico, a data e a hora que deseja agendar: ");
     this.pacienteid = read.nextInt();
     this.medicoid = read.nextInt();
+    read.nextLine();
     this.data = read.nextLine();
     this.hora = read.nextLine();
     System.out.println("consulta marcada! ");
     System.out.println("ID do médico: " + medicoid);
     System.out.println("Data: " + data + "\nHora: " + hora);
 }
-public void cancelar(int consultaid){
+public void cancelar(){
     System.out.println("Digite o ID da consulta para realizar o cancelamento: ");
     this.consultaid = read.nextInt();
+    read.nextLine();
     // if consultaid esta no banco de dados
     System.out.println("Consulta " + consultaid + " cancelada!");
     // caso contrario, System.out.println("ID inválido, verifique se foi realizado o agendamento");
 }
-public void buscar(int pacienteid, int medicoid, String data, String hora){
+public void buscar(){
     System.out.println("para buscar uma consulta, digite o id do paciente, id do médico, data e hora agendada: ");
     this.pacienteid = read.nextInt();
     this.medicoid = read.nextInt();
+    read.nextLine();
     this.data = read.nextLine();
     this.hora = read.nextLine();
     // se o id do paciente, id do médico, data e hora estiverem no banco de dados, a consulta aparece na tela
     // caso contrario, aparece "dados errados ou inexistentes"
 }
-
+public void fechar(){
+read.close();
+}
 }
